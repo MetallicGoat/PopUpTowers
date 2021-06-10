6054dfcb673bc44821ae01a10e889e4e898bf455
MetallicGoat <the.great.metallic.goat@gmail.com>
2021-06-09 20:58:16 -0400

    Made a PlaceBlock method

--- src/main/java/me/metallicgoat/PopUpTowers/BlockManager.java
+++ src/main/java/me/metallicgoat/PopUpTowers/BlockManager.java
@@ -2,6 +2,9 @@ package me.metallicgoat.PopUpTowers;
 
 import de.marcely.bedwars.api.BedwarsAPI;
 import de.marcely.bedwars.api.arena.Arena;
+import de.marcely.bedwars.api.arena.Team;
+import de.marcely.bedwars.api.game.spawner.Spawner;
+import de.marcely.bedwars.api.game.specialitem.SpecialItemUseSession;
 import me.metallicgoat.PopUpTowers.VersionSupport.BlockPlacer;
 import org.bukkit.*;
 import org.bukkit.block.Block;
@@ -13,34 +16,40 @@ import org.bukkit.material.Ladder;
 public class BlockManager {
     public BlockManager(Block b, int x, int y, int z, DyeColor color, Player p, boolean ladder, String ladderdata) {
         Main plugin = Main.getInstance();
-        BlockPlacer blockPlacer = plugin.blockPlacer;
+
 
         Arena arena = BedwarsAPI.getGameAPI().getArenaByPlayer(p);
 
-        int y2 = y + 1;
+
 
         //Is block there?
-        if (b.getRelative(x, y2, z).getType().equals(Material.AIR)) {
+        if (b.getRelative(x, y+1, z).getType().equals(Material.AIR)) {
 
             //Is block inside region
-            if (arena.isInside(b.getRelative(x, y2, z).getLocation())) {
-                if (!ladder) {
-                    blockPlacer.SetBlock(b, x, y2, z, color);
-                } else {
-                    b.getRelative(x, y2, z).setType(Material.LADDER);
-                    Block block = b.getRelative(x, y2, z);
-
-                    //Make this better (Error if ladder is not successfully placed from above. Ex. on a slab)
-                    if (block.getType() == Material.LADDER) {
-                        BlockState state = block.getState();
-                        Ladder lad = new Ladder();
-                        lad.setFacingDirection(BlockFace.valueOf(ladderdata));
-                        state.setData(lad);
-                        state.update();
-                    }
-                }
-                BedwarsAPI.getGameAPI().getArenaByPlayer(p).setBlockPlayerPlaced(b.getRelative(x, y2, z), true);
+            if (arena.isInside(b.getRelative(x, y+1, z).getLocation())) {
+                PlaceBlock(ladder, b, ladderdata, p, color, x, y+1, z);
             }
         }
+    }
+    private void PlaceBlock(boolean ladder, Block b, String ladderdata, Player p, DyeColor color, int x, int y, int z){
+        Main plugin = Main.getInstance();
+        Block block = b.getRelative(x, y, z);
+        BlockPlacer blockPlacer = plugin.blockPlacer;
+        if (!ladder) {
+            blockPlacer.SetBlock(b, x, y, z, color);
+        } else {
+            b.getRelative(x, y, z).setType(Material.LADDER);
+
+
+            //Make this better (Error if ladder is not successfully placed from above. Ex. on a slab)
+            if (block.getType() == Material.LADDER) {
+                BlockState state = block.getState();
+                Ladder lad = new Ladder();
+                lad.setFacingDirection(BlockFace.valueOf(ladderdata));
+                state.setData(lad);
+                state.update();
+            }
+        }
+        BedwarsAPI.getGameAPI().getArenaByPlayer(p).setBlockPlayerPlaced(b.getRelative(x, y, z), true);
     }
 }
