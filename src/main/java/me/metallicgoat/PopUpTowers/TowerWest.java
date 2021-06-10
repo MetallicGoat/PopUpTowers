57766d67163e9010fb400c07d7ba40e888a30874
MetallicGoat <the.great.metallic.goat@gmail.com>
2021-06-09 21:02:05 -0400

    Reworded block name

--- src/main/java/me/metallicgoat/PopUpTowers/TowerWest.java
+++ src/main/java/me/metallicgoat/PopUpTowers/TowerWest.java
@@ -6,19 +6,19 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 public class TowerWest {
-    public TowerWest(Location loc, Block chest, DyeColor color, Player p, SpecialItemUseSession session) {
+    public TowerWest(Location loc, Block clicked, DyeColor color, Player p, SpecialItemUseSession session) {
         Main plugin = Main.getInstance();
 
-        new BlockManager(chest, -2, 0, 1, color, p, false, "EAST");
-        new BlockManager(chest, -1, 0, 2, color, p, false, "EAST");
-        new BlockManager(chest, 0, 0, 2, color, p, false, "EAST");
-        new BlockManager(chest, 1, 0, 1, color, p, false, "EAST");
-        new BlockManager(chest, 1, 0, 0, color, p, false, "EAST");
-        new BlockManager(chest, 1, 0, -1, color, p, false, "EAST");
-        new BlockManager(chest, 0, 0, -2, color, p, false, "EAST");
-        new BlockManager(chest, -1, 0, -2, color, p, false, "EAST");
-        new BlockManager(chest, -2, 0, -1, color, p, false, "EAST");
-        new BlockManager(chest, 0, 0, 0, color, p, true, "EAST");
+        new BlockManager(clicked, -2, 0, 1, color, p, false, "EAST");
+        new BlockManager(clicked, -1, 0, 2, color, p, false, "EAST");
+        new BlockManager(clicked, 0, 0, 2, color, p, false, "EAST");
+        new BlockManager(clicked, 1, 0, 1, color, p, false, "EAST");
+        new BlockManager(clicked, 1, 0, 0, color, p, false, "EAST");
+        new BlockManager(clicked, 1, 0, -1, color, p, false, "EAST");
+        new BlockManager(clicked, 0, 0, -2, color, p, false, "EAST");
+        new BlockManager(clicked, -1, 0, -2, color, p, false, "EAST");
+        new BlockManager(clicked, -2, 0, -1, color, p, false, "EAST");
+        new BlockManager(clicked, 0, 0, 0, color, p, true, "EAST");
         if (Bukkit.getServer().getClass().getPackage().getName().contains("v1_8")) {
             loc.getWorld().playSound(loc, Sound.valueOf("CHICKEN_EGG_POP"), 1.0F, 1.0F);
         } else {
@@ -26,16 +26,16 @@ public class TowerWest {
         }
 
         Bukkit.getScheduler().runTaskLater(plugin, () -> {
-            new BlockManager(chest, -2, 1, 1, color, p, false, "EAST");
-            new BlockManager(chest, -1, 1, 2, color, p, false, "EAST");
-            new BlockManager(chest, 0, 1, 2, color, p, false, "EAST");
-            new BlockManager(chest, 1, 1, 1, color, p, false, "EAST");
-            new BlockManager(chest, 1, 1, 0, color, p, false, "EAST");
-            new BlockManager(chest, 1, 1, -1, color, p, false, "EAST");
-            new BlockManager(chest, 0, 1, -2, color, p, false, "EAST");
-            new BlockManager(chest, -1, 1, -2, color, p, false, "EAST");
-            new BlockManager(chest, -2, 1, -1, color, p, false, "EAST");
-            new BlockManager(chest, 0, 1, 0, color, p, true, "EAST");
+            new BlockManager(clicked, -2, 1, 1, color, p, false, "EAST");
+            new BlockManager(clicked, -1, 1, 2, color, p, false, "EAST");
+            new BlockManager(clicked, 0, 1, 2, color, p, false, "EAST");
+            new BlockManager(clicked, 1, 1, 1, color, p, false, "EAST");
+            new BlockManager(clicked, 1, 1, 0, color, p, false, "EAST");
+            new BlockManager(clicked, 1, 1, -1, color, p, false, "EAST");
+            new BlockManager(clicked, 0, 1, -2, color, p, false, "EAST");
+            new BlockManager(clicked, -1, 1, -2, color, p, false, "EAST");
+            new BlockManager(clicked, -2, 1, -1, color, p, false, "EAST");
+            new BlockManager(clicked, 0, 1, 0, color, p, true, "EAST");
             if (Bukkit.getServer().getClass().getPackage().getName().contains("v1_8")) {
                 loc.getWorld().playSound(loc, Sound.valueOf("CHICKEN_EGG_POP"), 1.0F, 1.0F);
             } else {
@@ -43,16 +43,16 @@ public class TowerWest {
             }
 
             Bukkit.getScheduler().runTaskLater(plugin, () -> {
-                new BlockManager(chest, -2, 2, 1, color, p, false, "EAST");
-                new BlockManager(chest, -1, 2, 2, color, p, false, "EAST");
-                new BlockManager(chest, 0, 2, 2, color, p, false, "EAST");
-                new BlockManager(chest, 1, 2, 1, color, p, false, "EAST");
-                new BlockManager(chest, 1, 2, 0, color, p, false, "EAST");
-                new BlockManager(chest, 1, 2, -1, color, p, false, "EAST");
-                new BlockManager(chest, 0, 2, -2, color, p, false, "EAST");
-                new BlockManager(chest, -1, 2, -2, color, p, false, "EAST");
-                new BlockManager(chest, -2, 2, -1, color, p, false, "EAST");
-                new BlockManager(chest, 0, 2, 0, color, p, true, "EAST");
+                new BlockManager(clicked, -2, 2, 1, color, p, false, "EAST");
+                new BlockManager(clicked, -1, 2, 2, color, p, false, "EAST");
+                new BlockManager(clicked, 0, 2, 2, color, p, false, "EAST");
+                new BlockManager(clicked, 1, 2, 1, color, p, false, "EAST");
+                new BlockManager(clicked, 1, 2, 0, color, p, false, "EAST");
+                new BlockManager(clicked, 1, 2, -1, color, p, false, "EAST");
+                new BlockManager(clicked, 0, 2, -2, color, p, false, "EAST");
+                new BlockManager(clicked, -1, 2, -2, color, p, false, "EAST");
+                new BlockManager(clicked, -2, 2, -1, color, p, false, "EAST");
+                new BlockManager(clicked, 0, 2, 0, color, p, true, "EAST");
                 if (Bukkit.getServer().getClass().getPackage().getName().contains("v1_8")) {
                     loc.getWorld().playSound(loc, Sound.valueOf("CHICKEN_EGG_POP"), 1.0F, 1.0F);
                 } else {
@@ -60,17 +60,17 @@ public class TowerWest {
                 }
 
                 Bukkit.getScheduler().runTaskLater(plugin, () -> {
-                    new BlockManager(chest, -2, 3, 0, color, p, false, "EAST");
-                    new BlockManager(chest, -2, 3, 1, color, p, false, "EAST");
-                    new BlockManager(chest, -1, 3, 2, color, p, false, "EAST");
-                    new BlockManager(chest, 0, 3, 2, color, p, false, "EAST");
-                    new BlockManager(chest, 1, 3, 1, color, p, false, "EAST");
-                    new BlockManager(chest, 1, 3, 0, color, p, false, "EAST");
-                    new BlockManager(chest, 1, 3, -1, color, p, false, "EAST");
-                    new BlockManager(chest, 0, 3, -2, color, p, false, "EAST");
-                    new BlockManager(chest, -1, 3, -2, color, p, false, "EAST");
-                    new BlockManager(chest, -2, 3, -1, color, p, false, "EAST");
-                    new BlockManager(chest, 0, 3, 0, color, p, true, "EAST");
+                    new BlockManager(clicked, -2, 3, 0, color, p, false, "EAST");
+                    new BlockManager(clicked, -2, 3, 1, color, p, false, "EAST");
+                    new BlockManager(clicked, -1, 3, 2, color, p, false, "EAST");
+                    new BlockManager(clicked, 0, 3, 2, color, p, false, "EAST");
+                    new BlockManager(clicked, 1, 3, 1, color, p, false, "EAST");
+                    new BlockManager(clicked, 1, 3, 0, color, p, false, "EAST");
+                    new BlockManager(clicked, 1, 3, -1, color, p, false, "EAST");
+                    new BlockManager(clicked, 0, 3, -2, color, p, false, "EAST");
+                    new BlockManager(clicked, -1, 3, -2, color, p, false, "EAST");
+                    new BlockManager(clicked, -2, 3, -1, color, p, false, "EAST");
+                    new BlockManager(clicked, 0, 3, 0, color, p, true, "EAST");
                     if (Bukkit.getServer().getClass().getPackage().getName().contains("v1_8")) {
                         loc.getWorld().playSound(loc, Sound.valueOf("CHICKEN_EGG_POP"), 1.0F, 1.0F);
                     } else {
@@ -78,26 +78,26 @@ public class TowerWest {
                     }
 
                     Bukkit.getScheduler().runTaskLater(plugin, () -> {
-                        new BlockManager(chest, 1, 4, 2, color, p, false, "EAST");
-                        new BlockManager(chest, 0, 4, 2, color, p, false, "EAST");
-                        new BlockManager(chest, -1, 4, 2, color, p, false, "EAST");
-                        new BlockManager(chest, -2, 4, 2, color, p, false, "EAST");
-                        new BlockManager(chest, 1, 4, 1, color, p, false, "EAST");
-                        new BlockManager(chest, 0, 4, 1, color, p, false, "EAST");
-                        new BlockManager(chest, -1, 4, 1, color, p, false, "EAST");
-                        new BlockManager(chest, -2, 4, 1, color, p, false, "EAST");
-                        new BlockManager(chest, 1, 4, 0, color, p, false, "EAST");
-                        new BlockManager(chest, -1, 4, 0, color, p, false, "EAST");
-                        new BlockManager(chest, -2, 4, 0, color, p, false, "EAST");
-                        new BlockManager(chest, 1, 4, -1, color, p, false, "EAST");
-                        new BlockManager(chest, 0, 4, -1, color, p, false, "EAST");
-                        new BlockManager(chest, -1, 4, -1, color, p, false, "EAST");
-                        new BlockManager(chest, -2, 4, -1, color, p, false, "EAST");
-                        new BlockManager(chest, 1, 4, -2, color, p, false, "EAST");
-                        new BlockManager(chest, 0, 4, -2, color, p, false, "EAST");
-                        new BlockManager(chest, -1, 4, -2, color, p, false, "EAST");
-                        new BlockManager(chest, -2, 4, -2, color, p, false, "EAST");
-                        new BlockManager(chest, 0, 4, 0, color, p, true, "EAST");
+                        new BlockManager(clicked, 1, 4, 2, color, p, false, "EAST");
+                        new BlockManager(clicked, 0, 4, 2, color, p, false, "EAST");
+                        new BlockManager(clicked, -1, 4, 2, color, p, false, "EAST");
+                        new BlockManager(clicked, -2, 4, 2, color, p, false, "EAST");
+                        new BlockManager(clicked, 1, 4, 1, color, p, false, "EAST");
+                        new BlockManager(clicked, 0, 4, 1, color, p, false, "EAST");
+                        new BlockManager(clicked, -1, 4, 1, color, p, false, "EAST");
+                        new BlockManager(clicked, -2, 4, 1, color, p, false, "EAST");
+                        new BlockManager(clicked, 1, 4, 0, color, p, false, "EAST");
+                        new BlockManager(clicked, -1, 4, 0, color, p, false, "EAST");
+                        new BlockManager(clicked, -2, 4, 0, color, p, false, "EAST");
+                        new BlockManager(clicked, 1, 4, -1, color, p, false, "EAST");
+                        new BlockManager(clicked, 0, 4, -1, color, p, false, "EAST");
+                        new BlockManager(clicked, -1, 4, -1, color, p, false, "EAST");
+                        new BlockManager(clicked, -2, 4, -1, color, p, false, "EAST");
+                        new BlockManager(clicked, 1, 4, -2, color, p, false, "EAST");
+                        new BlockManager(clicked, 0, 4, -2, color, p, false, "EAST");
+                        new BlockManager(clicked, -1, 4, -2, color, p, false, "EAST");
+                        new BlockManager(clicked, -2, 4, -2, color, p, false, "EAST");
+                        new BlockManager(clicked, 0, 4, 0, color, p, true, "EAST");
                         if (Bukkit.getServer().getClass().getPackage().getName().contains("v1_8")) {
                             loc.getWorld().playSound(loc, Sound.valueOf("CHICKEN_EGG_POP"), 1.0F, 1.0F);
                         } else {
@@ -105,44 +105,44 @@ public class TowerWest {
                         }
 
                         Bukkit.getScheduler().runTaskLater(plugin, () -> {
-                            new BlockManager(chest, -2, 4, 3, color, p, false, "EAST");
-                            new BlockManager(chest, -2, 5, 3, color, p, false, "EAST");
-                            new BlockManager(chest, -2, 6, 3, color, p, false, "EAST");
-                            new BlockManager(chest, -1, 5, 3, color, p, false, "EAST");
-                            new BlockManager(chest, 0, 5, 3, color, p, false, "EAST");
-                            new BlockManager(chest, 1, 4, 3, color, p, false, "EAST");
-                            new BlockManager(chest, 1, 5, 3, color, p, false, "EAST");
-                            new BlockManager(chest, 1, 6, 3, color, p, false, "EAST");
-                            new BlockManager(chest, -2, 4, -3, color, p, false, "EAST");
-                            new BlockManager(chest, -2, 5, -3, color, p, false, "EAST");
-                            new BlockManager(chest, -2, 6, -3, color, p, false, "EAST");
-                            new BlockManager(chest, -1, 5, -3, color, p, false, "EAST");
-                            new BlockManager(chest, 0, 5, -3, color, p, false, "EAST");
-                            new BlockManager(chest, 1, 4, -3, color, p, false, "EAST");
-                            new BlockManager(chest, 1, 5, -3, color, p, false, "EAST");
-                            new BlockManager(chest, 1, 6, -3, color, p, false, "EAST");
-                            new BlockManager(chest, 2, 4, 2, color, p, false, "EAST");
-                            new BlockManager(chest, 2, 5, 2, color, p, false, "EAST");
-                            new BlockManager(chest, 2, 6, 2, color, p, false, "EAST");
-                            new BlockManager(chest, 2, 5, 1, color, p, false, "EAST");
-                            new BlockManager(chest, 2, 4, 0, color, p, false, "EAST");
-                            new BlockManager(chest, 2, 5, 0, color, p, false, "EAST");
-                            new BlockManager(chest, 2, 6, 0, color, p, false, "EAST");
-                            new BlockManager(chest, 2, 5, -1, color, p, false, "EAST");
-                            new BlockManager(chest, 2, 4, -2, color, p, false, "EAST");
-                            new BlockManager(chest, 2, 5, -2, color, p, false, "EAST");
-                            new BlockManager(chest, 2, 6, -2, color, p, false, "EAST");
-                            new BlockManager(chest, -3, 4, 2, color, p, false, "EAST");
-                            new BlockManager(chest, -3, 5, 2, color, p, false, "EAST");
-                            new BlockManager(chest, -3, 6, 2, color, p, false, "EAST");
-                            new BlockManager(chest, -3, 5, 1, color, p, false, "EAST");
-                            new BlockManager(chest, -3, 4, 0, color, p, false, "EAST");
-                            new BlockManager(chest, -3, 5, 0, color, p, false, "EAST");
-                            new BlockManager(chest, -3, 6, 0, color, p, false, "EAST");
-                            new BlockManager(chest, -3, 5, -1, color, p, false, "EAST");
-                            new BlockManager(chest, -3, 4, -2, color, p, false, "EAST");
-                            new BlockManager(chest, -3, 5, -2, color, p, false, "EAST");
-                            new BlockManager(chest, -3, 6, -2, color, p, false, "EAST");
+                            new BlockManager(clicked, -2, 4, 3, color, p, false, "EAST");
+                            new BlockManager(clicked, -2, 5, 3, color, p, false, "EAST");
+                            new BlockManager(clicked, -2, 6, 3, color, p, false, "EAST");
+                            new BlockManager(clicked, -1, 5, 3, color, p, false, "EAST");
+                            new BlockManager(clicked, 0, 5, 3, color, p, false, "EAST");
+                            new BlockManager(clicked, 1, 4, 3, color, p, false, "EAST");
+                            new BlockManager(clicked, 1, 5, 3, color, p, false, "EAST");
+                            new BlockManager(clicked, 1, 6, 3, color, p, false, "EAST");
+                            new BlockManager(clicked, -2, 4, -3, color, p, false, "EAST");
+                            new BlockManager(clicked, -2, 5, -3, color, p, false, "EAST");
+                            new BlockManager(clicked, -2, 6, -3, color, p, false, "EAST");
+                            new BlockManager(clicked, -1, 5, -3, color, p, false, "EAST");
+                            new BlockManager(clicked, 0, 5, -3, color, p, false, "EAST");
+                            new BlockManager(clicked, 1, 4, -3, color, p, false, "EAST");
+                            new BlockManager(clicked, 1, 5, -3, color, p, false, "EAST");
+                            new BlockManager(clicked, 1, 6, -3, color, p, false, "EAST");
+                            new BlockManager(clicked, 2, 4, 2, color, p, false, "EAST");
+                            new BlockManager(clicked, 2, 5, 2, color, p, false, "EAST");
+                            new BlockManager(clicked, 2, 6, 2, color, p, false, "EAST");
+                            new BlockManager(clicked, 2, 5, 1, color, p, false, "EAST");
+                            new BlockManager(clicked, 2, 4, 0, color, p, false, "EAST");
+                            new BlockManager(clicked, 2, 5, 0, color, p, false, "EAST");
+                            new BlockManager(clicked, 2, 6, 0, color, p, false, "EAST");
+                            new BlockManager(clicked, 2, 5, -1, color, p, false, "EAST");
+                            new BlockManager(clicked, 2, 4, -2, color, p, false, "EAST");
+                            new BlockManager(clicked, 2, 5, -2, color, p, false, "EAST");
+                            new BlockManager(clicked, 2, 6, -2, color, p, false, "EAST");
+                            new BlockManager(clicked, -3, 4, 2, color, p, false, "EAST");
+                            new BlockManager(clicked, -3, 5, 2, color, p, false, "EAST");
+                            new BlockManager(clicked, -3, 6, 2, color, p, false, "EAST");
+                            new BlockManager(clicked, -3, 5, 1, color, p, false, "EAST");
+                            new BlockManager(clicked, -3, 4, 0, color, p, false, "EAST");
+                            new BlockManager(clicked, -3, 5, 0, color, p, false, "EAST");
+                            new BlockManager(clicked, -3, 6, 0, color, p, false, "EAST");
+                            new BlockManager(clicked, -3, 5, -1, color, p, false, "EAST");
+                            new BlockManager(clicked, -3, 4, -2, color, p, false, "EAST");
+                            new BlockManager(clicked, -3, 5, -2, color, p, false, "EAST");
+                            new BlockManager(clicked, -3, 6, -2, color, p, false, "EAST");
                             if (Bukkit.getServer().getClass().getPackage().getName().contains("v1_8")) {
                                 loc.getWorld().playSound(loc, Sound.valueOf("CHICKEN_EGG_POP"), 1.0F, 1.0F);
                             } else {
